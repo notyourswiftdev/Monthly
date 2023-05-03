3e5d830fccae24f4d918f28fa1be2a19a67acf0f
Aaron Cleveland <toscleveland@gmail.com>
2023-05-03 09:35:57 -0500

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Aaron Cleveland on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
