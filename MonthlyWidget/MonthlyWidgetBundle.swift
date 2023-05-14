ea6dbe7eb3d9949f8c773ee949e88e644cccf1d1
Aaron Cleveland <toscleveland@gmail.com>
2023-05-14 14:38:37 -0500

    Adjusted logic for the avatar image download.

---
+++ MonthlyWidget/MonthlyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyWidgetBundle.swift
+//  MonthlyWidget
+//
+//  Created by Aaron Cleveland on 5/3/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MonthlyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MonthlyWidget()
+    }
+}
